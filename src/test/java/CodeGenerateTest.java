74e1a489ce86c9f0b98720844d1fe689e9f665ca
zzw <1103047590@qq.com>
2021-01-27 11:22:02 +0800

    代码生成器

--- src/test/java/CodeGenerateTest.java
+++ src/test/java/CodeGenerateTest.java
@@ -116,7 +116,7 @@ public class CodeGenerateTest {
                     @Override
                     public String outputFile(TableInfo tableInfo) {
                         // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
-                        return projectPath + "/template/src/main/resources/mapping/" + pc.getModuleName()
+                        return projectPath + "/src/main/resources/mapper/" + pc.getModuleName()
                                 + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
                     }
                 });
